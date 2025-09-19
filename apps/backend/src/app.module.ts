ba7cc36f57c84f2cf705d2941d3e0a7f19ab8a34
lavoide <fedyok24@gmail.com>
2025-09-19 09:00:09 +0000

    feat: frontend changes

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ActivitiesModule } from './activity/activities.module';
@@ -9,6 +11,10 @@ import { HelpersModule } from './shared/helpers/helpers.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'frontend', 'dist'),
+      exclude: ['/api*'],
+    }),
     UsersModule,
     AuthModule,
     ActivitiesModule,
--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,24 +1,16 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+    <div className="page">
+      <header className="header">
+        <h1>Trainbook</h1>
+      </header>
+      <main className="content">
         <ExploreContainer />
-      </IonContent>
-    </IonPage>
+      </main>
+    </div>
   );
 };
 
