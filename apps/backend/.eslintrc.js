fe51da1e53ad640d7010b8f2bcadd7474eaee30b
lavoide <fedyok24@gmail.com>
2025-10-31 08:25:49 +0000

    feat: import sorting

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'simple-import-sort'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,35 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-var': 'error',
+    'prefer-const': 'warn',
+    'import/newline-after-import': ['error', { count: 1 }],
+    'import/no-duplicates': ['error', { considerQueryString: true }],
+    'simple-import-sort/exports': 'error',
+    'sort-imports': [
+      'error',
+      {
+        ignoreDeclarationSort: true,
+        allowSeparatedGroups: true,
+        ignoreCase: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index'], ['type'], ['unknown']],
+        pathGroups: [{ pattern: '@/**', group: 'internal', position: 'after' }],
+        pathGroupsExcludedImportTypes: ['builtin', 'type'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports', disallowTypeAnnotations: false },
+    ],
   },
 };
--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,7 +1,10 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import eslintPluginImport from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -16,6 +19,9 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: eslintPluginImport,
+      prettier,
+      'simple-import-sort': eslintPluginSimpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -25,6 +31,38 @@ export default tseslint.config(
       ],
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-var': 'error',
+      'prefer-const': 'warn',
+      'prettier/prettier': 'error',
+      'import/newline-after-import': ['error', { count: 1 }],
+      'import/no-duplicates': ['error', { considerQueryString: true }],
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'sort-imports': [
+        'error',
+        {
+          ignoreDeclarationSort: true,
+          allowSeparatedGroups: true,
+          ignoreCase: true,
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index'], ['type'], ['unknown']],
+          pathGroups: [{ pattern: '@/**', group: 'internal', position: 'after' }],
+          pathGroupsExcludedImportTypes: ['builtin', 'type'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports', disallowTypeAnnotations: false },
+      ],
     },
   },
 )
